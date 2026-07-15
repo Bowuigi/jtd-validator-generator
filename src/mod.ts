128133d9319b8a7bb6cc10465f3908e9d9a1ef87
Bowuigi <bowuigi@vern.cc>
2026-07-15 01:20:48 -0300

    feat: Implement most of the type form, except for timestamp

--- src/mod.ts
+++ src/mod.ts
@@ -48,6 +48,72 @@ export type Schema = {
   definitions?: Record<string, SomeForm>
 } & SomeForm;
 
+const interpolatedTypeOfData = '${data === null ? "null" : typeof data}';
+
+function generateType(type: TypeForm['type']): CG.AST {
+  const ranges = {
+    float32: { min: -3.40282347e+38, max: 3.4028234663852886e+38 },
+    int8: { min: -128, max: 127 },
+    uint8: { min: 0, max: 255 },
+    int16: { min: -32768, max: 32767 },
+    uint16: { min: 0, max: 65535 },
+    int32: { min: -2147483648, max: 2147483647 },
+    uint32: { min: 0, max: 4294967295 }
+  } as const;
+
+  switch (type) {
+    case 'string':
+    case 'boolean':
+      return CG.unless(
+        CG.dataIs(type),
+        CG.pushError(`expected ${type}, got ${interpolatedTypeOfData}`, CG.array([]))
+      );
+
+    case 'timestamp':
+      return CG.pushError('timestamp NYI', CG.array([]));
+
+    // NOTE: NaN and +/- Infinity do not exist in JSON, so this is the same as Number.isFinite
+    case 'float64':
+      return CG.unless(
+        CG.dataIs('number'),
+        CG.pushError(`expected ${type}, got ${interpolatedTypeOfData}`, CG.array([]))
+      );
+
+    case 'float32':
+      return CG.ifElse(
+        CG.dataIs('number'),
+        CG.unless(
+          `(data >= ${ranges[type].min} && data <= ${ranges[type].max})`,
+          CG.pushError(`value \${data} out of range for ${type}`, CG.array([]))
+        ),
+        CG.pushError(`expected ${type}, got ${interpolatedTypeOfData}`, CG.array([]))
+      );
+
+    case 'int16':
+    case 'int32':
+    case 'int8':
+    case 'uint16':
+    case 'uint32':
+    case 'uint8':
+      return CG.ifElse(
+        CG.dataIs('number'),
+        CG.ifElse(
+          `(data >= ${ranges[type].min} && data <= ${ranges[type].max})`,
+          CG.unless(
+            'Number.isInteger(data)',
+            CG.pushError(
+              `value \${data} is not an ${type.startsWith('u') ? 'unsigned ' : ''}integer`,
+              CG.array([])
+            )
+          ),
+          CG.pushError(`value \${data} out of range for ${type}`, CG.array([]))
+        ),
+        CG.pushError(`expected ${type}, got ${interpolatedTypeOfData}`, CG.array([]))
+      );
+  }
+  return CG.pushError('NYI', CG.array([]));
+}
+
 function generateEnum(options: Array<string>): CG.AST {
   return CG.formBlock(
     'enum',
@@ -55,7 +121,7 @@ function generateEnum(options: Array<string>): CG.AST {
     CG.ifElse(
       CG.dataIs('string'),
       CG.unless('enum_.includes(data)', CG.pushError('unexpected "${data}"', 'enum_')),
-      CG.pushError('unexpected ${data === null ? "null" : typeof data}', 'enum_')
+      CG.pushError(`unexpected ${interpolatedTypeOfData}`, 'enum_')
     )
   );
 }
@@ -69,6 +135,8 @@ function onForm(form: SomeForm): CG.AST {
 
   if ('enum' in form) {
     appended.push(generateEnum(form.enum));
+  } else if ('type' in form) {
+    appended.push(generateType(form.type));
   }
 
   if (appended.length === 0) {
